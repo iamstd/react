7aa4ccaf8dede6433d1eb98045b4f813553c4d85
iamstd <chunhua.zheng@haiziwang.com>
2016-10-13 16:47:12 +0800

    add

---
+++ reducers/addtodo.js
@@ -0,0 +1,10 @@
+import order from '../actions/order';
+
+function todoList (state = initialState, action) {
+  switch(action.type){
+    case todoList:
+    return action.index
+  }
+}
+
+export default todoList;
